9d69a1cf78a1bb9ae076ac64fec14a48f03dd62a
github username <github email address>
2018-02-27 16:11:55 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 
 import CocktailsPageContainer from '../containers/CocktailsPageContainer'
+import LoginForm from './LoginForm'
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+    username: ""
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault()
+    this.setState({
+      loggedIn: true,
+      username: event.target[0].value.toUpperCase()
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Boozer</h2>
+          <h2>Welcome to Boozer {this.state.username}</h2>
+          {this.state.loggedIn ? null : <LoginForm handleLogin={this.handleLogin}/>}
+          <Link to='/'>Home</Link><br/>
           <Link to='/cocktails'>See All Cocktails</Link>
-          <Link to='/'>Home</Link>
         </div>
         <Route path="/cocktails" component={CocktailsPageContainer} />
       </div>
